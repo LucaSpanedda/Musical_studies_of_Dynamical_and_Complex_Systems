9ebd52e70b001c331142f6f3d6ddd62d272bb464
Luca_Spanedda <stigmadubstep@gmail.com>
2021-12-13 19:58:28 +0100

    Update 1.51_pdwavepy_chaos-function.py

--- 1.51_pdwavepy_chaos-function.py
+++ 1.51_pdwavepy_chaos-function.py
@@ -1,6 +1,10 @@
 # CAOS PROCESS inside the - ARRAY- for Pure Data
 
 
+# Import the math Library (for sin and pi)
+import math
+
+
 # Initializing the patch
 # Standard definitions
 canvas = print("#N canvas 605 244 388 332 10;")
@@ -19,10 +23,9 @@ print("#A")
 # -------- CAOS PROCESS INSIDE ARRAY
 # PROCESSO:
 # la funzione caosprocess(a) è il PROCESSO DETERMINISTICO MATEMATICO 
-# su cui si vuole fare - feedback - reiterazione dell'output
-# qui radice quadrata di a (una potenza a 0,5 corrisponde matematicamente alla radice quadrata)
+# su cui si vuole fare - feedback - reiterazione dell'output: a
 def caosprocess(a):
-    return pow(a,1.005)
+    return (a*0.99)
 
 # ITERAZIONI:
 # è definito qui quante iterazioni ripete il processo di caos deterministico:
@@ -30,14 +33,15 @@ iterazioni = int(513)
 
 # VALORE DI PARTENZA:
 # valore di partenza del processo caos deterministico:
-valore= float(2)
+valore= float(1)
 
 for j in range (0,iterazioni):
     # mando il valore nel caosprocess
     out=caosprocess(valore)
     
-    # REGOLAZIONE: nel print regolo i valori dell'out in modo che siano fra 0 ed 1
-    print(out / 8000)
+    # REGOLAZIONE: nel print regolo i valori dell'out guidando la funzione SINE
+    # I VALORI IN out DAL PROCESSO funzionano da FASORE
+    print(math.sin(out*(math.pi*2)))
     # feedback: - prendo l'uscita da out e la reimmetto nel caosprocess
     valore=out
     
